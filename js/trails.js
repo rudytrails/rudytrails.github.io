64d32038ea8380b09baeb36a01698bdbdf901169
rudymemo <rudydiff@126.com>
2021-04-12 23:42:12 +0800

    修改文本内容

--- js/trails.js
+++ js/trails.js
@@ -19,25 +19,25 @@ map.setMapStyleV2({
 
 var points = [
 	// 北京
-	{"lng":116.404, "lat":39.925, "title":"故宫", "img":"img/place/tianAnMen.jpg", "content":"第一次去北京跟我哥一起去的，很多人排队，当时有租学生证的，买票会便宜不少，哈哈！"} 
-	,{"lng":116.303548, "lat":40.007763, "title":"圆明园遗址", "img":"img/place/beijing/tianAnMen.jpg", "content":""}
-	,{"lng":116.188382, "lat":39.990412, "title":"香山公园", "img":"img/place/beijing/IMG_0736.JPG", "content":"🍁🍁🍁呢？"}
+	{"lng":116.404, "lat":39.925, "title":"故宫", "img":"img/place/beijing/IMG_1416.JPG", "content":"第一次去北京跟我哥一起去的，很多人排队，当时有租学生证的，买票会便宜不少，哈哈！"} 
+	,{"lng":116.303548, "lat":40.007763, "title":"圆明园遗址", "img":"img/place/beijing/IMG_0943.JPG", "content":"对雕刻艺术表示尊敬，不过没有近距离观察，放一张园内的风景吧。"}
+	,{"lng":116.188382, "lat":39.990412, "title":"香山公园", "img":"img/place/beijing/IMG_0736.JPG", "content":"🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁呢？"}
 	,{"lng":116.296524, "lat":40.154127, "title":"盛辉公寓", "img":"img/place/beijing/IMG_0562.JPG", "content":"刚来北京时候的落脚点。这是周边的农贸市场，现在估计应该没了吧。"}
 	,{"lng":116.264333, "lat":40.165005, "title":"北街家园", "img":"img/place/beijing/tianAnMen.jpg", "content":"从盛辉公寓搬到北街家园，听说郭德纲姚明在前面的玫瑰园有房子，可惜没时间去拜访~~"}
-	,{"lng":116.294679, "lat":40.264869, "title":"莽山森林公园", "img":"img/place/beijing/tianAnMen.jpg", "content":"美好的回忆"}
+	,{"lng":116.294679, "lat":40.264869, "title":"莽山森林公园", "img":"img/place/beijing/tianAnMen.jpg", "content":"自由总是与痛苦相伴，幸福却往往失去自由。大多数人都是选择走第二条路。"}
 	,{"lng":116.262774, "lat":40.259433, "title":"十三陵水库", "img":"img/place/beijing/IMG_1536.JPG", "content":"曾驾驭小黄车绕水库骑行，与别人的山地车和公路自行车格格不入，回头率反正不低，贼尴尬~然后傻傻的去追逐落日。"}
 	,{"lng":117.277318, "lat":40.654251, "title":"古北水镇", "img":"img/place/beijing/IMG_0601.JPG", "content":"来北京第一年公司组织团建去的地方，风景不错，有古北水镇、司马台长城。第一次玩三国杀，现在完全忘记怎么玩了。"}
-	,{"lng":117.277318, "lat":40.654251, "title":"居庸关", "img":"img/place/beijing/IMG_1107.JPG", "content":"从下面看很雄伟，爬的时候也很陡，最陡的地方几乎要直上直下了。"}
+	,{"lng":116.079159, "lat":40.296972, "title":"居庸关", "img":"img/place/beijing/IMG_1107.JPG", "content":"从下面看很雄伟，爬的时候也很陡，最陡的地方几乎要直上直下了。"}
 	// 秦皇岛
-	,{"lng":119.516368, "lat":39.815773, "title":"北戴河", "img":"img/place/beijing/IMG_2120.JPG", "content":"跟俩二黑去沙滩玩了两天，回来之后耳朵发炎了，估计是海水的原因，持续了很长时间。。。"}
+	,{"lng":119.541332, "lat":39.826987, "title":"北戴河", "img":"img/place/beijing/IMG_2120.JPG", "content":"汪国真在《假如你不够快乐》中写到：假如你不够快乐，也不要把眉头深锁，人生本来短暂，为什么还要栽培苦涩？打开尘封的门窗，让阳光雨露洒遍每个角落，走向生命的原野，让风儿熨平前额。"}
 	// 内蒙古
-	,{"lng":117.14279, "lat":42.58022, "title":"将军泡子景区", "img":"img/place/dalian/theairofcat.jpg", "content":"好朋友来北京玩，带去草原转了一圈，这是他辞职创业的开始，祝好。"}
+	,{"lng":117.227612, "lat":42.602345, "title":"乌兰布统风景区", "img":"img/place/beijing/IMG_2169.JPG", "content":"好朋友来北京玩，带去草原转了一圈，这是他辞职创业的开始，祝好。"}
 	// 承德
-	,{"lng":117.464597, "lat":40.594467, "title":"雾灵山风景区", "img":"img/place/beijing/IMG_0659.JPG", "content":"天然氧吧。下山的时候不注意滑了一跤，摔的不轻 - -"}
+	,{"lng":117.464597, "lat":40.594467, "title":"雾灵山风景区", "img":"img/place/beijing/IMG_0659.JPG", "content":"天然氧吧。下山的时候不注意滑了一跤，摔的不轻，不过没事，那时候还年轻呢！"}
 	
 	// 大连
-	,{"lng":121.588799, "lat":38.876344, "title":"星海湾", "img":"img/place/dalian/xinghaiwan.jpg", "content":"美好的回忆"}
-	,{"lng":121.693195, "lat":38.932155, "title":"猫の天空之城", "img":"img/place/dalian/theairofcat.jpg", "content":"猫の天空之城概念书店，可以撸猫哦"}
+	,{"lng":121.588799, "lat":38.876344, "title":"星海湾", "img":"img/place/dalian/xinghaiwan.jpg", "content":"我们所有人的当下，都只是行走在未来的飘忽不定的记忆中罢了。"}
+	,{"lng":121.693195, "lat":38.932155, "title":"猫の天空之城", "img":"img/place/dalian/theairofcat.jpg", "content":"猫の天空之城概念书店，可以撸猫哦。类似的还有喵星球、猫小院～～"}
 	
 	// 济南
 	
